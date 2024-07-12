7543838a431e3c3ee0c9262a4a752edc28e20061
Vineeth05kumar <Vineeth05kumar>
2024-07-12 16:10:56 +0530

    Feature to add movies AND console it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
-
+import AddMovie from "./components/AddMovie";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
@@ -7,15 +7,17 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [retrying, setRetrying] = useState(false);
+  // const [retrying, setRetrying] = useState(false);
+  // const [openForm, setOpenForm] = useState(false);
+
   const fetchMovies = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/film");
+      const response = await fetch("https://swapi.dev/api/films");
       if (!response.ok) {
         throw new Error("Something went wrong....RETRYING!!!");
-        setRetrying(true);
+        // setRetrying(true);
       }
       const data = await response.json();
       const transformedMovies = data.results.map((movieData) => {
@@ -31,10 +33,10 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-    if (retrying) {
-      setTimeout(fetchMovies, 5000);
-    }
-  }, [retrying]);
+    // if (retrying) {
+    //   setTimeout(fetchMovies, 5000);
+    // }
+  }, []);
 
   useEffect(() => {
     fetchMovies();
@@ -52,17 +54,23 @@ function App() {
     content = <p>Loading...</p>;
   }
 
-  const cancelSetTimeout = () => {
-    setRetrying(false);
+  // const cancelSetTimeout = () => {
+  //   setRetrying(false);
+  // };
+  const addMovies = (obj) => {
+    console.log(obj);
   };
 
   return (
     <React.Fragment>
       <section>
+        <AddMovie onAddMovie={addMovies} />
+      </section>
+      <section>
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>{content}</section>
-      <button onClick={cancelSetTimeout}>CANCEL_RETRYING</button>
+      {/* <button onClick={cancelSetTimeout}>CANCEL_RETRYING</button> */}
     </React.Fragment>
   );
 }
